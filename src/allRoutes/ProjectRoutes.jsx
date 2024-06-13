bef89ccf383724b4db2874f70feb3303a13919f3
abhik345 <abhikdas345@gmail.com>
2024-06-13 18:27:50 +0530

    added framer motion loader

--- src/allRoutes/ProjectRoutes.jsx
+++ src/allRoutes/ProjectRoutes.jsx
@@ -1,16 +1,30 @@
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import {Home} from "../pages/index.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "../pages/index.js";
+import { useEffect, useState } from "react";
+import Loading from "../components/loading/Loading.jsx";
 const ProjectRoutes = () => {
-    return(
-        <>
+  const [isLoading, setIsLoading] = useState(true);
+ 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
         <Router>
-            <Routes>
-                <Route path = "/" element={<Home/>} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
         </Router>
-        </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-
-export default ProjectRoutes
+export default ProjectRoutes;
