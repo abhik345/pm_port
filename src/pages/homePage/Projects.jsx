29ac41cbf1d6c0d72038a299348c300613e3a4ac
abhik345 <abhikdas345@gmail.com>
2024-06-21 12:32:27 +0530

    image removed video added

--- src/pages/homePage/Projects.jsx
+++ src/pages/homePage/Projects.jsx
@@ -9,11 +9,10 @@ function Projects() {
   const headRef1 = useRef(null);
   const headRef2 = useRef(null);
 
-  const images = [
-    "http://static.showit.co/800/M0IzUCMvQCqlJn1YtNlikw/59514/pexels-yan-5793641-1.jpg",
-    "http://static.showit.co/800/137NFxuzSxeyjlons2WEzA/59514/pexels-yan-5793643.jpg",
-    "http://static.showit.co/800/3kOLYaOCS1SMieN6Y88Fqg/59514/mukuko-studio-mu88mlefcou-unsplash.jpg",
-
+  const videos = [
+    "https://cruisefront.kreativemachinez.in/images/videoo-ship.mp4",
+    "https://cruisefront.kreativemachinez.in/images/videoo-ship.mp4",
+    "https://cruisefront.kreativemachinez.in/images/videoo-ship.mp4",
   ];
 
   useGSAP(() => {
@@ -45,17 +44,17 @@ function Projects() {
   useEffect(() => {
     let ctx = gsap.context(() => {
       gsap.set(".photo:not(:last-child)", {
-        clipPath: function (){
-          return "inset(0px 0px 0px 0px)"
-        }
+        clipPath: function () {
+          return "inset(0px 0px 0px 0px)";
+        },
       });
 
       const animation = gsap.to(".photo:not(:last-child)", {
-        clipPath: function (){
-          return "inset(0px 0px 100% 0px)"
+        clipPath: function () {
+          return "inset(0px 0px 100% 0px)";
         },
         stagger: 0.5,
-        ease : "none"
+        ease: "none",
       });
 
       ScrollTrigger.create({
@@ -71,139 +70,154 @@ function Projects() {
   }, []);
 
   return (
-      <section className="bg-black">
-        <div className="gallery container mx-auto flex">
-          <div className="left w-1/3 ml-auto">
-            <div className=" bg-black mt-10 px-2 py-6">
-              <h3 ref={headRef1} className="cite-with-line text-[20px] text-white font-medium">
-                Projects
-              </h3>
-              <h2 ref={headRef2} className="main-heading text-[50px] font-bold mb-4">
-                <span className="text-[#959595]">Lets</span>
-                <span className="text-white"> Empower Business Growth</span>
-              </h2>
+    <section className="bg-black">
+      <div className="gallery container mx-auto flex">
+        <div className="left w-1/3 ml-auto">
+          <div className=" bg-black mt-10 px-2 py-6">
+            <h3
+              ref={headRef1}
+              className="cite-with-line text-[20px] text-white font-medium"
+            >
+              Projects
+            </h3>
+            <h2
+              ref={headRef2}
+              className="main-heading text-[50px] font-bold mb-4"
+            >
+              <span className="text-[#959595]">Lets</span>
+              <span className="text-white"> Empower Business Growth</span>
+            </h2>
+          </div>
+          <div className="details h-[60vh] flex flex-col justify-center w-full ml-auto text-white text-3xl font-extrabold">
+            <p className="mt-4 text-xl font-semibold underline underline-offset-8">
+              Ride the Digital Wave
+            </p>
+            <div className="flex items-center justify-between">
+              <p className="mt-6 text-[#DCA514]">Section I of XVI</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="64"
+                height="64"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrow-up-right"
+              >
+                <path d="M7 7h10v10" />
+                <path d="M7 17 17 7" />
+              </svg>
             </div>
-            <div className="details h-[60vh] flex flex-col justify-center w-full ml-auto text-white text-3xl font-extrabold">
-              <p className="mt-4 text-xl font-semibold underline underline-offset-8">Ride the Digital Wave</p>
-              <div className="flex items-center justify-between">
-                <p className="mt-6 text-[#DCA514]">Section I of XVI</p>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="64"
-                    height="64"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="white"
-                    strokeWidth="1"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-arrow-up-right"
-                >
-                  <path d="M7 7h10v10" />
-                  <path d="M7 17 17 7" />
-                </svg>
-              </div>
-              <div className="mt-2 font-semibold">
-                <span>Crossing The Digital</span>
-                <span> Frontier</span>
-              </div>
-              <div className="mt-2 text-[16px] font-normal start-gen-pop">
-                <div className="font-normal">1. AN OVERVIEW OF THE LANDSCAPE</div>
-                <ul className="list-disc list-inside">
-                  <li className="ml-5">Surveying the scenario</li>
-                  <li className="ml-5">The Strategic Road Map</li>
-                  <li className="ml-5">Understanding the Strategic Road Map</li>
-                </ul>
-              </div>
-              
+            <div className="mt-2 font-semibold">
+              <span>Crossing The Digital</span>
+              <span> Frontier</span>
             </div>
-            <div className="details mt-40 h-[60vh] flex flex-col justify-center w-full ml-auto text-white text-3xl font-extrabold">
-              <p className="mt-4 text-xl font-semibold underline underline-offset-8">Ride the Digital Wave</p>
-              <div className="flex items-center justify-between">
-                <p className="mt-6 text-[#DCA514]">Section I of XVI</p>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="64"
-                    height="64"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="white"
-                    strokeWidth="1"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-arrow-up-right"
-                >
-                  <path d="M7 7h10v10" />
-                  <path d="M7 17 17 7" />
-                </svg>
-              </div>
-              <div className="mt-2 font-semibold">
-                <span>Crossing The Digital</span>
-                <span> Frontier</span>
-              </div>
-              <div className="mt-2 text-[16px] font-normal">
-                <div>1. AN OVERVIEW OF THE LANDSCAPE</div>
-                <ul className="list-disc list-inside">
-                  <li className="ml-5">Surveying the scenario</li>
-                  <li className="ml-5">The Strategic Road Map</li>
-                  <li className="ml-5">Understanding the Strategic Road Map</li>
-                </ul>
-              </div>
-             
-            </div>
-            <div className="details mt-40 mb-20 h-[60vh] flex flex-col justify-center w-full ml-auto text-white text-3xl font-extrabold">
-              <p className="mt-4 text-xl font-semibold underline underline-offset-8">Ride the Digital Wave</p>
-              <div className="flex items-center justify-between">
-                <p className="mt-6 text-[#DCA514]">Section I of XVI</p>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="64"
-                    height="64"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="white"
-                    strokeWidth="1"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-arrow-up-right"
-                >
-                  <path d="M7 7h10v10" />
-                  <path d="M7 17 17 7" />
-                </svg>
-              </div>
-              <div className="mt-2 font-semibold">
-                <span>Crossing The Digital</span>
-                <span> Frontier</span>
-              </div>
-              <div className="mt-2 text-[16px] font-normal">
-                <div>1. AN OVERVIEW OF THE LANDSCAPE</div>
-                <ul className="list-disc list-inside">
-                  <li className="ml-5">Surveying the scenario</li>
-                  <li className="ml-5">The Strategic Road Map</li>
-                  <li className="ml-5">Understanding the Strategic Road Map</li>
-                </ul>
-              </div>
-              
+            <div className="mt-2 text-[16px] font-normal start-gen-pop">
+              <div className="font-normal">1. AN OVERVIEW OF THE LANDSCAPE</div>
+              <ul className="list-disc list-inside">
+                <li className="ml-5">Surveying the scenario</li>
+                <li className="ml-5">The Strategic Road Map</li>
+                <li className="ml-5">Understanding the Strategic Road Map</li>
+              </ul>
             </div>
           </div>
-          <br />
-          <div className="rightblock w-2/3 h-screen flex flex-col justify-center">
-            <div className="relative w-[600px] h-[600px] left-56">
-              {images.map((image, index) => (
-                  <div key={index} className={`photo absolute w-full h-full flex flex-row gap-6`} style={{
-                    zIndex : 100 - index
-                   }}>
-                    <img
-                        src={image}
-                        alt={`img-${index}`}
-                        className="w-full h-full rounded-3xl"
-                    />
-                  </div>
-              ))}
+          <div className="details mt-40 h-[60vh] flex flex-col justify-center w-full ml-auto text-white text-3xl font-extrabold">
+            <p className="mt-4 text-xl font-semibold underline underline-offset-8">
+              Ride the Digital Wave
+            </p>
+            <div className="flex items-center justify-between">
+              <p className="mt-6 text-[#DCA514]">Section I of XVI</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="64"
+                height="64"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrow-up-right"
+              >
+                <path d="M7 7h10v10" />
+                <path d="M7 17 17 7" />
+              </svg>
+            </div>
+            <div className="mt-2 font-semibold">
+              <span>Crossing The Digital</span>
+              <span> Frontier</span>
+            </div>
+            <div className="mt-2 text-[16px] font-normal">
+              <div>1. AN OVERVIEW OF THE LANDSCAPE</div>
+              <ul className="list-disc list-inside">
+                <li className="ml-5">Surveying the scenario</li>
+                <li className="ml-5">The Strategic Road Map</li>
+                <li className="ml-5">Understanding the Strategic Road Map</li>
+              </ul>
+            </div>
+          </div>
+          <div className="details mt-40 mb-20 h-[60vh] flex flex-col justify-center w-full ml-auto text-white text-3xl font-extrabold">
+            <p className="mt-4 text-xl font-semibold underline underline-offset-8">
+              Ride the Digital Wave
+            </p>
+            <div className="flex items-center justify-between">
+              <p className="mt-6 text-[#DCA514]">Section I of XVI</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="64"
+                height="64"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrow-up-right"
+              >
+                <path d="M7 7h10v10" />
+                <path d="M7 17 17 7" />
+              </svg>
+            </div>
+            <div className="mt-2 font-semibold">
+              <span>Crossing The Digital</span>
+              <span> Frontier</span>
+            </div>
+            <div className="mt-2 text-[16px] font-normal">
+              <div>1. AN OVERVIEW OF THE LANDSCAPE</div>
+              <ul className="list-disc list-inside">
+                <li className="ml-5">Surveying the scenario</li>
+                <li className="ml-5">The Strategic Road Map</li>
+                <li className="ml-5">Understanding the Strategic Road Map</li>
+              </ul>
             </div>
           </div>
         </div>
-      </section>
+        <br />
+        <div className="rightblock w-2/3 h-screen flex flex-col justify-center">
+          <div className="relative w-[600px] h-[600px] left-56">
+            {videos.map((video, index) => (
+              <div
+                key={index}
+                className={`photo absolute w-full h-full flex flex-row gap-6`}
+                style={{ zIndex: 100 - index }}
+              >
+                <video
+                  src={video}
+                  alt={`video-${index}`}
+                  className="w-full h-full rounded-3xl object-cover"
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
